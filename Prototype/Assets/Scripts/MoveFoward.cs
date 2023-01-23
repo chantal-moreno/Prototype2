26d56f7c6b77f6aff978f62a2eb33c448b054916
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2023-01-23 17:59:00 -0600

    Add dogs prefabs

--- Prototype/Assets/Scripts/MoveFoward.cs
+++ Prototype/Assets/Scripts/MoveFoward.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveFoward : MonoBehaviour
 {
     public float speed = 10.0f;
-    public float spin = 250.0f;
+    private float spin = 250.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +15,14 @@ public class MoveFoward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up * Time.deltaTime * spin);
-        transform.position += new Vector3(0,0,1) * Time.deltaTime * speed;
+        if (gameObject.CompareTag("Projectile"))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * spin);
+            transform.position += new Vector3(0,0,1) * Time.deltaTime * speed;
+        }
+        if (gameObject.CompareTag("Dog"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
     }
 }
