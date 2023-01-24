be8ac5363f83958f31907151e54550e78a71df05
chantal-moreno <77076572+chantal-moreno@users.noreply.github.com>
2023-01-23 18:32:18 -0600

    Update SpawnManager

--- Prototype/Assets/Scripts/SpawnManager.cs
+++ Prototype/Assets/Scripts/SpawnManager.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] dogsPrefabs;
+    public float spawnRangeX = 10;
+    public float spawnPosZ = 20;
+    public float startDelay = 2;
+    public float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void SpawnRandomAnimal(){
+        int animalIndex = Random.Range(0, dogsPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(dogsPrefabs[animalIndex], spawnPos, dogsPrefabs[animalIndex].transform.rotation);
     }
 }
